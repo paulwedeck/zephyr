c198b6bafc2f7f14a15b0021ecb1efc9d86917db
Paul Wedeck <paulwedeck@gmail.com>
2025-04-11 15:07:15 +0200

    initial version of wch uart dma driver (unfinished)

--- drivers/serial/uart_wch_usart.c
+++ drivers/serial/uart_wch_usart.c
@@ -12,7 +12,14 @@
 #include <zephyr/drivers/pinctrl.h>
 #include <zephyr/irq.h>
 
+#ifdef CONFIG_UART_ASYNC_API
+#include <zephyr/drivers/dma.h>
+#endif
+
 #include <ch32fun.h>
+
+
+static void usart_wch_rx_timeout(struct k_work *work);
 
 struct usart_wch_config {
 	USART_TypeDef *regs;
@@ -24,16 +31,44 @@ struct usart_wch_config {
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	void (*irq_config_func)(const struct device *dev);
 #endif
+#ifdef CONFIG_UART_ASYNC_API
+	struct {
+		const struct device *dev;
+		uint32_t chan;
+	} dma_rx, dma_tx;
+#endif
 };
 
+#ifdef CONFIG_UART_ASYNC_API
+struct usart_wch_rx_state {
+	struct k_work_delayable dwork;
+	const struct device *uart_dev;
+	uint8_t* active_bfr;
+	size_t active_bfr_len;
+	uint8_t* next_bfr;
+	size_t next_bfr_len;
+};
+#endif
+
 struct usart_wch_data {
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	uart_irq_callback_user_data_t cb;
 	void *user_data;
+#endif
+#ifdef CONFIG_UART_ASYNC_API
+	uart_callback_t async_cb;
+	void *async_user_data;
+	uint8_t *tx_buf;
+	size_t tx_size;
+
+	struct usart_wch_rx_data rx_state;
+#endif
 };
 
 static int usart_wch_init(const struct device *dev)
 {
 	const struct usart_wch_config *config = dev->config;
+	struct usart_wch_data *data = dev->data;
 	USART_TypeDef *regs = config->regs;
 	uint32_t ctlr1 = USART_CTLR1_TE | USART_CTLR1_RE | USART_CTLR1_UE;
 	uint32_t clock_rate;
@@ -76,6 +111,10 @@ static int usart_wch_init(const struct device *dev)
 	config->irq_config_func(dev);
 #endif
 
+	data->rx_state.uart_dev = dev;
+
+	k_work_init_delayable(&data->rx_state.dwork, usart_wch_rx_timeout);
+
 	return 0;
 }
 
@@ -86,6 +125,10 @@ static int usart_wch_poll_in(const struct device *dev, unsigned char *ch)
 
 	if ((regs->STATR & USART_STATR_RXNE) == 0) {
 		return -1;
+	}
+
+	if (regs->CTLR3 & USART_CTLR3_DMAR) {
+		return -EBUSY;
 	}
 
 	*ch = regs->DATAR;
@@ -281,6 +324,230 @@ static void usart_wch_irq_callback_set(const struct device *dev, uart_irq_callba
 }
 #endif /*CONFIG_UART_INTERRUPT_DRIVEN*/
 
+#ifdef CONFIG_UART_ASYNC_API
+
+static void usart_wch_dma_tx_callback(const struct device *dev, void *user_data, uint32_t channel,
+				      int status)
+{
+	const struct device *uart_dev = user_data;
+	const struct usart_wch_config *config = uart_dev->config;
+	struct usart_wch_data *data = uart_dev->data;
+	USART_TypeDef *regs = config->regs;
+	struct uart_event event = {
+		.type = UART_TX_DONE,
+		.data.tx.buf = data->tx_buf,
+		.data.tx.len = data->tx_size,
+	};
+
+	regs->CTLR3 &= ~USART_CTLR3_DMAT;
+	data->tx_size = 0;
+	data->async_cb(uart_dev, &event, data->async_user_data);
+}
+
+static int usart_wch_callback_set(const struct device *dev, uart_callback_t callback,
+				  void *user_data)
+{
+	struct usart_wch_data *data = dev->data;
+
+	data->async_cb = callback;
+	data->async_user_data = user_data;
+
+	return 0;
+}
+
+static int usart_wch_tx(const struct device *dev, const uint8_t *buf, size_t len, int32_t timeout)
+{
+	const struct usart_wch_config *config = dev->config;
+	struct usart_wch_data *data = dev->data;
+	USART_TypeDef *regs = config->regs;
+
+	struct dma_block_config dma_blk = {
+		.source_address = (uint32_t)buf,
+		.dest_address = (uint32_t)&regs->DATAR,
+		.source_addr_adj = DMA_ADDR_ADJ_INCREMENT,
+		.dest_addr_adj = DMA_ADDR_ADJ_NO_CHANGE,
+		.block_size = len,
+	};
+	struct dma_config dma_cfg = {
+		.channel_direction = MEMORY_TO_PERIPHERAL,
+		.head_block = &dma_blk,
+		.block_count = 1,
+		.source_data_size = 1,
+		.dest_data_size = 1,
+		.user_data = (void *)dev,
+		.dma_callback = usart_wch_dma_tx_callback,
+	};
+	int result;
+	unsigned key;
+
+	if(!data->async_cb) {
+		return -EINVAL;
+	}
+
+	if (timeout != SYS_FOREVER_US) {
+		return -EINVAL;
+	}
+
+	key = irq_lock();
+
+	if (data->tx_size > 0) {
+		result = -EBUSY;
+		goto out;
+	}
+
+	result = dma_config(config->dma_tx.dev, config->dma_tx.chan, &dma_cfg);
+	if (result < 0) {
+		goto out;
+	}
+
+	result = dma_start(config->dma_tx.dev, config->dma_tx.chan);
+	if (result < 0) {
+		goto out;
+	}
+
+	regs->CTLR3 |= USART_CTLR3_DMAT;
+	data->tx_size = len;
+	data->tx_buf = (uint8_t *)buf;
+
+out:
+	irq_unlock(key);
+	return result;
+}
+
+static int usart_wch_tx_abort(const struct device *dev)
+{
+	const struct usart_wch_config *config = dev->config;
+	struct usart_wch_data *data = dev->data;
+	USART_TypeDef *regs = config->regs;
+	struct dma_status status;
+	uint32_t ctlr3;
+	unsigned key;
+	struct uart_event event = {
+		.type = UART_TX_ABORTED,
+	};
+
+	key = irq_lock();
+	ctlr3 = regs->CTLR3;
+	regs->CTLR3 = ctlr3&~USART_CTLR3_DMAT;
+	irq_unlock(key);
+
+	if(!(ctlr3 & USART_CTLR3_DMAT)) {
+		return -EFAULT;
+	}
+
+	dma_get_status(config->dma_tx.dev, config->dma_tx.chan, &status);
+	dma_stop(config->dma_tx.dev, config->dma_tx.chan);
+
+	event.data.tx.buf = data->tx_buf;
+	event.data.tx.len = data->tx_size - status.pending_length;
+	data->tx_size = 0;
+
+	data->async_cb(dev, &event, data->async_user_data);
+	return 0;
+}
+
+static void usart_wch_dma_rx_callback(const struct device *dev, void *user_data, uint32_t channel,
+	int status)
+{
+
+}
+
+static void usart_wch_rx_timeout(struct k_work *work)
+{
+
+}
+
+static int usart_wch_rx_start_transmit(struct usart_wch_rx_state *rx_state)
+{
+	const struct device *dev = rx_state->uart_dev;
+	const struct usart_wch_config *config = dev->config;
+	USART_TypeDef *regs = config->regs;
+	int result;
+
+	struct dma_block_config dma_blk = {
+		.source_address = (uint32_t)&regs->DATAR,
+		.dest_address = (uint32_t)rx_state->next_bfr,
+		.source_addr_adj = DMA_ADDR_ADJ_INCREMENT,
+		.dest_addr_adj = DMA_ADDR_ADJ_NO_CHANGE,
+		.block_size = rx_state->next_bfr_len,
+	};
+	struct dma_config dma_cfg = {
+		.channel_direction = PERIPHERAL_TO_MEMORY,
+		.head_block = &dma_blk,
+		.block_count = 1,
+		.source_data_size = 1,
+		.dest_data_size = 1,
+		.user_data = (void *)dev,
+		.dma_callback = usart_wch_dma_rx_callback,
+	};
+
+	result = dma_config(config->dma_rx.dev, config->dma_rx.chan, &dma_cfg);
+	if (result < 0) {
+		goto out;
+	}
+
+	result = dma_start(config->dma_rx.dev, config->dma_rx.chan);
+	if (result < 0) {
+		goto out;
+	}
+
+	rx_state->active_bfr = rx_state->next_bfr;
+	rx_state->active_bfr_len = rx_state->next_bfr_len;
+	rx_state->next_bfr = NULL;
+	rx_state->next_bfr_len = 0;
+
+
+out:
+	return result;
+}
+
+static int usart_wch_rx_enable(const struct device *dev, uint8_t *buf, size_t len, int32_t timeout)
+{
+	const struct usart_wch_config *config = dev->config;
+	struct usart_wch_data *data = dev->data;
+	USART_TypeDef *regs = config->regs;
+	int result;
+	unsigned key;
+
+	if(!data->async_cb) {
+		return -EINVAL;
+	}
+
+	key = irq_lock();
+
+	if (data->rx_state.active_bfr) {
+		result = -EBUSY;
+		goto out;
+	}
+
+	data->rx_state.next_bfr = buf;
+	data->rx_state.next_bfr_size = len;
+
+	regs->CTLR3 |= USART_CTLR3_DMAR;
+	usart_wch_rx_start_transmit(&data->rx_state);
+out:
+	irq_unlock(key);
+	return result;
+}
+
+static int usart_wch_rx_buf_rsp(const struct device *dev, uint8_t *buf, size_t len)
+{
+	return 0; // TODO
+}
+
+static int usart_wch_rx_disable(const struct device *dev)
+{
+	const struct usart_wch_config *config = dev->config;
+	struct usart_wch_data *data = dev->data;
+	USART_TypeDef *regs = config->regs;
+	struct dma_status status;
+	uint32_t ctlr3;
+	unsigned key;
+	struct uart_event event = {};
+}
+
+#endif /*CONFIG_UART_ASYNC_API*/
+
 static DEVICE_API(uart, usart_wch_driver_api) = {
 	.poll_in = usart_wch_poll_in,
 	.poll_out = usart_wch_poll_out,
@@ -301,6 +568,14 @@ static DEVICE_API(uart, usart_wch_driver_api) = {
 	.irq_update = usart_wch_irq_update,
 	.irq_callback_set = usart_wch_irq_callback_set,
 #endif
+#ifdef CONFIG_UART_ASYNC_API
+	.callback_set = usart_wch_callback_set,
+	.tx = usart_wch_tx,
+	.tx_abort = usart_wch_tx_abort,
+	.rx_enable = usart_wch_rx_enable,
+	.rx_buf_rsp = usart_wch_rx_buf_rsp,
+	.rx_disable = usart_wch_rx_disable,
+#endif
 };
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
@@ -312,7 +587,7 @@ static DEVICE_API(uart, usart_wch_driver_api) = {
 #define USART_WCH_IRQ_HANDLER(idx)                                                                 \
 	static void usart_wch_irq_config_func_##idx(const struct device *dev)                      \
 	{                                                                                          \
-		IRQ_CONNECT(DT_INST_IRQN(idx), DT_INST_IRQ(idx, priority), usart_wch_isr,        \
+		IRQ_CONNECT(DT_INST_IRQN(idx), DT_INST_IRQ(idx, priority), usart_wch_isr,          \
 			    DEVICE_DT_INST_GET(idx), 0);                                           \
 		irq_enable(DT_INST_IRQN(idx));                                                     \
 	}
@@ -320,6 +595,17 @@ static DEVICE_API(uart, usart_wch_driver_api) = {
 #define USART_WCH_IRQ_HANDLER_DECL(idx)
 #define USART_WCH_IRQ_HANDLER_FUNC(idx)
 #define USART_WCH_IRQ_HANDLER(idx)
+#endif
+
+#ifdef CONFIG_UART_ASYNC_API
+#define USART_WCH_DMA_DEVICES(idx)                                                                 \
+	.dma_rx.dev = DEVICE_DT_GET(DT_DMAS_CTLR_BY_NAME(DT_DRV_INST(idx), rx)),                   \
+	.dma_rx.chan = DT_DMAS_CELL_BY_NAME(DT_DRV_INST(idx), rx, channel),                        \
+	.dma_tx.dev = DEVICE_DT_GET(DT_DMAS_CTLR_BY_NAME(DT_DRV_INST(idx), tx)),                   \
+	.dma_tx.chan = DT_DMAS_CELL_BY_NAME(DT_DRV_INST(idx), tx, channel),
+
+#else
+#define USART_WCH_DMA_DEVICES(idx)
 #endif
 
 #define USART_WCH_INIT(idx)                                                                        \
@@ -333,7 +619,7 @@ static DEVICE_API(uart, usart_wch_driver_api) = {
 		.clock_dev = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(idx)),                              \
 		.clock_id = DT_INST_CLOCKS_CELL(idx, id),                                          \
 		.pin_cfg = PINCTRL_DT_INST_DEV_CONFIG_GET(idx),                                    \
-		USART_WCH_IRQ_HANDLER_FUNC(idx)};                                                  \
+		USART_WCH_IRQ_HANDLER_FUNC(idx) USART_WCH_DMA_DEVICES(idx)};                       \
 	DEVICE_DT_INST_DEFINE(idx, &usart_wch_init, NULL, &usart_wch_##idx##_data,                 \
 			      &usart_wch_##idx##_config, PRE_KERNEL_1,                             \
 			      CONFIG_SERIAL_INIT_PRIORITY, &usart_wch_driver_api);                 \
